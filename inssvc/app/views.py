b95bccbf7be5ac1800fef7c11b090a83607a5626
Chris Fanucchi <admin@knightskeep.com>
2023-02-08 01:07:33 +0000

    Added bootstrap framework

--- inssvc/app/views.py
+++ inssvc/app/views.py
@@ -10,39 +10,39 @@ from .forms import EmailForm
 
 load_dotenv()
 
-# def index_view(request):
-#     return render(request, "index.html")
+def base_view(request):
+    return render(request, "base.html")
 
 
 def form_view(request):
-    if request.POST:
-        form = EmailForm(request.POST)
-        print(request.POST)
-        if form.is_valid():
-            print('POST successful!')
+    # if request.POST:
+    #     form = EmailForm(request.POST)
+    #     print(request.POST)
+    #     if form.is_valid():
+    #         print('POST successful!')
 
-        try:
-            file_path = Path(__file__).parent.resolve()
-            html = Template(
-                Path(f'{file_path}/email_content.html').read_text())
-            email = EmailMessage()
-            email['from'] = "test@knightskeep.com"
-            email['to'] = "admin@knightskeep.com"
-            email['subject'] = "From Contact Us Form"
+    #     try:
+    #         file_path = Path(__file__).parent.resolve()
+    #         html = Template(
+    #             Path(f'{file_path}/email_content.html').read_text())
+    #         email = EmailMessage()
+    #         email['from'] = "test@knightskeep.com"
+    #         email['to'] = "admin@knightskeep.com"
+    #         email['subject'] = "From Contact Us Form"
 
-            date_time = timezone.localtime().strftime("%m/%d/%Y %H:%M:%S (PT)")
-            token, name, email_address, message = request.POST.values()
-            email.set_content(html.substitute(name=name, email=email_address,
-                                              message=message, sent=date_time), 'html')
+    #         date_time = timezone.localtime().strftime("%m/%d/%Y %H:%M:%S (PT)")
+    #         token, name, email_address, message = request.POST.values()
+    #         email.set_content(html.substitute(name=name, email=email_address,
+    #                                           message=message, sent=date_time), 'html')
 
-            with smtplib.SMTP(host='encore.websitewelcome.com', port=587) as smtp:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.login(os.getenv('USER_NAME'), os.getenv('PASS'))
-                smtp.send_message(email)
-                print('Email sent!')
+    #         with smtplib.SMTP(host='encore.websitewelcome.com', port=587) as smtp:
+    #             smtp.ehlo()
+    #             smtp.starttls()
+    #             smtp.login(os.getenv('USER_NAME'), os.getenv('PASS'))
+    # smtp.send_message(email)
+    # print('Email sent!')
 
-        except Exception as err:
-            print('Something went wrong with send_mail. Error: ' + str(err))
+    # except Exception as err:
+    #     print('Something went wrong with send_mail. Error: ' + str(err))
 
     return render(request, 'index.html', {'form': EmailForm})
--- inssvc/inssvc/settings.py
+++ inssvc/inssvc/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites'
+    'django.contrib.sites',
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'inssvc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "app/templates")],
+        'DIRS': [BASE_DIR / "app/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,19 +126,77 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app/static")
+    BASE_DIR / "app/static"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+BOOTSTRAP5 = {
+
+    # The complete URL to the Bootstrap CSS file
+    # Note that a URL can be either a string,
+    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/5.1.1/css/bootstrap.min.css",
+    # or a dict like the default value below.
+    "css_url": {
+        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+        "integrity": "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
+        "crossorigin": "anonymous",
+    },
+
+    # The complete URL to the Bootstrap JavaScript file
+    "javascript_url": {
+        "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
+        "integrity": "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T",
+        "crossorigin": "anonymous",
+    },
+
+    # The complete URL to the Bootstrap CSS file (None means no theme)
+    "theme_url": None,
+
+    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap5.html)
+    'javascript_in_head': False,
+
+    # Label class to use in horizontal forms
+    'horizontal_label_class': 'col-md-3',
+
+    # Field class to use in horizontal forms
+    'horizontal_field_class': 'col-md-9',
+
+    # Set placeholder attributes to label if no placeholder is provided
+    'set_placeholder': True,
+
+    # Class to indicate required (better to set this in your Django form)
+    'required_css_class': '',
+
+    # Class to indicate error (better to set this in your Django form)
+    'error_css_class': 'is-invalid',
+
+    # Class to indicate success, meaning the field has valid input (better to set this in your Django form)
+    'success_css_class': 'is-valid',
+
+    # Renderers (only set these if you have studied the source and understand the inner workings)
+    'formset_renderers': {
+        'default': 'bootstrap5.renderers.FormsetRenderer',
+    },
+    'form_renderers': {
+        'default': 'bootstrap5.renderers.FormRenderer',
+    },
+    'field_renderers': {
+        'default': 'bootstrap5.renderers.FieldRenderer',
+        'inline': 'bootstrap5.renderers.InlineFieldRenderer',
+    },
+}
+
 # *** SECURITY SETTINGS ***
 
 # * Settings from django-csp *
 CSP_DEFAULT_SRC = ("'self'",)
-CSP_STYLE_SRC = ("'self'",)
-CSP_SCRIPT_SRC = ("'self'",)
+CSP_STYLE_SRC = (
+    "'self'", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css")
+CSP_SCRIPT_SRC = (
+    "'self'", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js")
 CSP_IMG_SRC = ("'self'",)
 CSP_FONT_SRC = ("'self'",)
 
